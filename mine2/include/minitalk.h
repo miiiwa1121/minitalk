89199e6edb38d39015fb9b8bd4b3db08f74c04c3
Miwa Tsubasa <mtsubasa@c6r1s4.42tokyo.jp>
2024-12-22 10:33:59 +0900

    ok

--- mine2/include/minitalk.h
+++ mine2/include/minitalk.h
@@ -6,14 +6,14 @@
 /*   By: mtsubasa <mtsubasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 04:33:00 by mtsubasa          #+#    #+#             */
-/*   Updated: 2024/12/21 05:05:41 by mtsubasa         ###   ########.fr       */
+/*   Updated: 2024/12/22 10:09:16 by mtsubasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "../other/include/libft.h"
+# include "libft.h"
 # include <signal.h>
 
 typedef struct sigaction	t_sa;
--- mine2/include/minitalk_bonus.h
+++ mine2/include/minitalk_bonus.h
@@ -6,14 +6,14 @@
 /*   By: mtsubasa <mtsubasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 04:32:51 by mtsubasa          #+#    #+#             */
-/*   Updated: 2024/12/21 05:05:53 by mtsubasa         ###   ########.fr       */
+/*   Updated: 2024/12/22 10:09:14 by mtsubasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
-# include "../other/include/libft.h"
+# include "libft.h"
 # include <signal.h>
 
 typedef struct sigaction	t_sa;

173cf51071430acc2beaf39cb07b1d97c55d9500
mtsubasa <k023c0026@g.neec.ac.jp>
2025-03-23 01:53:39 +0900

    mvn

--- report/server_bonus.c
+++ report/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mtsubasa <mtsubasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 15:22:14 by jtakahas          #+#    #+#             */
-/*   Updated: 2025/03/22 17:13:33 by mtsubasa         ###   ########.fr       */
+/*   Updated: 2025/03/23 01:42:32 by mtsubasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,15 @@ void	signal_handler(int signal, siginfo_t *info, void *context)
 		if (info->si_pid > 0) // NULL チェック
 			kill(info->si_pid, signal);// クライアントにACK（確認応答）を送信
 		//（SIGUSR1 または SIGUSR2）を返す。
-
+		//si_pid は、siginfo_t 構造体のメンバで、シグナルを送信したプロセスのプロセス ID (PID) を表します。
+		//`sa_flags`に`SA_SIGINFO`を設定すると、`sigaction`でこの情報を取得できます。
+		//「->」はC言語におけるポインタを使った構造体メンバーへのアクセスを表す記法
+		//通常の構造体変数の場合はドット記法（.）を使いますが、ポインタの場合は -> を使います。
+		//info_ptr->si_pid は実際には (*info_ptr).si_pid と同じ意味ですが、コードの可読性を考慮して -> を使います。
+		
 		// kill(info->si_pid, signal);
 	}
-	(void)context;
+	(void)context;//未使用の引数を無視
 }
 
 int	main(int ac, char **av)
